c85aa98045cc8ea360b61018f679c3b7f09f5ee4
HpDima <136693475+HpDima@users.noreply.github.com>
2023-07-02 23:14:14 +0300

    Update l.html

--- в.py
+++ в.py
@@ -1,21 +1,34 @@
-"""def rer(d):
-    for f in set(d):
-        count = 0
-        for f1 in d:
-            if f == f1:
-                count += 1
-        print(f'{count} - {f}')
-rer('abcnra')
-"""
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>2z</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+<a href="fersrt.html" target="_blank">Переход</a>
+<h1> Страница 1</h1>
+<form action="">
+  <label for="fidef">Логие/Номер</label>
+  <input class="form-label" id="fidef" type="text" placeholder="88">
+  <label for="fide">Пороль</label>
+  <input id="fide"type="password">
+  <label for="fide5">Запомнить меня</label>
+  <input  class="form-check-input" id="fide5" type="checkbox">
 
+  <input type="week">
+  <input type="date">
+  <input type="range">
 
-def rer(d):
-    syms_con = {}
-    for sym in d:
-        syms_con[sym] = syms_con.get(sym, 0) + 1
-    for sym, count in syms_con.items():
-        print(sym, count)
-
-rer('ageyerytogyuerwy')
+  <input type="datetime-local">
+  <input type="tel">
+  <input class="btn btn-danger"  type="button" value="Войти">
+  <textarea cols="20" rows="3"></textarea>
+</form>
+</body>
+</html>
 
 
